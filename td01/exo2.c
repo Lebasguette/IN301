939a827f186fb7419f1a7d5fcd60a2b02457f588
Lebataclan <adam-lebas@orange.fr>
2016-10-13 10:21:35 +0200

    TD3

--- td01/exo2.c
+++ td01/exo2.c
@@ -2,16 +2,5 @@
 
 int main()
 {
-	int a,b,c,d;
-	
-	printf("saisir le nombre de secondes \n");
-	scanf("%d", &a);
-	b = a/3600;
-	c = (a%3600)/60;
-	d = a%60;
-	printf("celà fait : \n %d h %d m %d s \n", b, c,d); 
-	
-
-
-
+	printf("Taille int %lu \n", sizeof(int));
 }
